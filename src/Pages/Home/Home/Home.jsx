e57c073a179e4be10f90f2ee8fe576a27a235e85
alamin360am <alaminhimu0@gmail.com>
2023-06-28 15:40:26 +0600

    home section complete

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,14 +1,32 @@
-import SectionHeading from '../../../Components/SectionHeading/SectionHeading';
-import ProfilePhoto from '../ProfilePhoto/ProfilePhoto';
-import './home.css'
+import { Link } from "react-router-dom";
+import SectionHeading from "../../../Components/SectionHeading/SectionHeading";
+import ProfilePhoto from "../ProfilePhoto/ProfilePhoto";
+import "./home.css";
 
 const Home = () => {
-    return (
-        <section>
-            <SectionHeading heading={"Home"}></SectionHeading>
-            <ProfilePhoto></ProfilePhoto>
-        </section>
-    );
+  return (
+    <section className="home">
+      <SectionHeading heading={"Home"}></SectionHeading>
+      <div className="grid grid-2-col">
+        <div className="hero">
+          <h3 className="secondary-heading">
+            Hi, I am <span>Al - Amin</span>
+          </h3>
+          <p className="subtitle">A full stack web developer</p>
+          <p className="description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut placeat
+            commodi aut omnis, at reprehenderit quaerat veniam, quam consectetur
+            hic id tempore sit temporibus accusamus dolorum assumenda expedita
+            dicta libero?
+          </p>
+          <Link to='/about' className="btn">
+            More about me
+          </Link>
+        </div>
+        <ProfilePhoto></ProfilePhoto>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- src/Pages/Home/ProfilePhoto/ProfilePhoto.jsx
+++ src/Pages/Home/ProfilePhoto/ProfilePhoto.jsx
@@ -1,11 +1,20 @@
-import './profile-photo.css';
+import "./profile-photo.css";
+import profileImg from "./../../../../public/Profile-Photo.jpg";
 
 const ProfilePhoto = () => {
-    return (
-        <div>
-            al
-        </div>
-    );
+  const title = "Al Amin Rahmatullah - MERN stack web developer - "
+  const letter = title.split('')
+  console.log(letter);
+  return (
+    <div className="profile-img-box">
+      <div className="img">
+        <img src={profileImg} alt="" />
+      </div>
+      <div className="text">{
+        letter.map((char, i)=><span key={char} style={{transform: `rotate(${i*7.5}deg)`}}>{char}</span>)
+      }</div>
+    </div>
+  );
 };
 
 export default ProfilePhoto;
